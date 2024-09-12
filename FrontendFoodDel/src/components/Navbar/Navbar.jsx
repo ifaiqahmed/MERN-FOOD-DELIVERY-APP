da2211ecd1fdbbb66497dfcff086ad19a67fada2
Denis Murphy <faiqaws1234@gmail.com>
2024-09-12 13:05:11 +0500

    Frontend connected with backend

--- FrontendFoodDel/src/components/Navbar/Navbar.jsx
+++ FrontendFoodDel/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,21 @@
 import React, { useContext, useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { StoreContext } from "../../Context/StoreContext";
 
 const Navbar = ({ setShowLogin }) => {
-  const [menu, setMenu, token, setToken] = useState("home");
-  const {getTotalCartAmount} = useContext(StoreContext)
+  const [menu, setMenu] = useState("home");
+  const {getTotalCartAmount, token, setToken } = useContext(StoreContext)
+
+  const navigate = useNavigate();
+  const logout = ()=>{
+    localStorage.removeItem("token");
+    setToken("");
+    navigate("/");
+
+  }
+
   return (
     <div className="navbar">
       <Link to="/">
@@ -42,7 +51,6 @@ const Navbar = ({ setShowLogin }) => {
           contact us
         </a>
       </ul>
-
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
@@ -51,7 +59,20 @@ const Navbar = ({ setShowLogin }) => {
           </Link>
           <div className={getTotalCartAmount()===0?"":"dot"} ></div>
         </div>
-        {!token?<button onClick={() => setShowLogin(true)}>sign in</button>:<div> <img src={assets.profile_icon} alt="" /></div>
+        {!token?<button onClick={() => setShowLogin(true)}>sign in</button>:
+       <div className="navbar-profile">
+       <img src={assets.profile_icon} alt="Profile Icon" />
+       <ul className="navbar-profile-dropdown">
+         <li>
+           <img src={assets.bag_icon} alt="Orders Icon" /> <p>Orders</p>
+         </li>
+         <hr />
+         <li onClick={logout}>
+           <img src={assets.logout_icon} alt="Logout Icon" /> <p>Logout</p>
+         </li>
+       </ul>
+     </div>
+
 }
       </div>
     </div>
--- FrontendFoodDel/src/pages/Cart/Cart.jsx
+++ FrontendFoodDel/src/pages/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import "./Cart.css";
 import { StoreContext } from "../../Context/StoreContext";
 import { useNavigate } from 'react-router-dom'
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart,getTotalCartAmount } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart,getTotalCartAmount, url } = useContext(StoreContext);
   const navigate = useNavigate();
   return (
     <div className="cart">
@@ -22,7 +22,7 @@ const Cart = () => {
           if (cartItems[item._id] > 0) {
             return (
               <div className="cart-items-title cart-items-item">
-                <img src={item.image} alt="" />
+                <img src={url+"/images/"+item.image} alt="" />
                 <p>{item.name}</p>
                 <p>${item.price}</p>
                 <p>{cartItems[item._id]}</p>
